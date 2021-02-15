d7afb6c6dc4a04b42114653d831255eb2adfb746
Jeremy Teo <jimmy.teo878@gmail.com>
2021-02-15 21:21:29 +0800

    Merge remote-tracking branch 'upstream/generics_cage'
    # Conflicts:
    #	src/main/java/circus/Circus.java

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,12 +1,11 @@
 package circus;
 
-import circus.animal.*;
+import circus.animal.Animal;
+import circus.animal.Duck;
+import circus.animal.Parrot;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Circus {
     private static Animal[] animals = {
@@ -40,47 +39,8 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-//        makeAnimalsTalk();
-//        System.out.println("Total value of animals " + calculateAssetValue(animals));
-//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
-
-        System.out.println("Number of animals: " + animals.length); // len of animals array
-
-        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
-            // to convert array to arrayList - need to first convert to list
-
-        // add new animal
-        animalArrayList.add(new Tiger("Tigger"));
-        System.out.println("Number of animals: " + animalArrayList.size()); // len of animals array
-
-        // add duck and elephant
-        Duck louie = new Duck("Louie");
-        animalArrayList.add(louie);
-
-        Elephant Dumbo = new Elephant("Dumbo");
-        animalArrayList.add(Dumbo);
-        System.out.println("Number of animals: " + animalArrayList.size()); // len of animals array
-
-        printAllAnimals(animalArrayList);
-
-        // find the position of the object in arraylist - indexof
-        System.out.println("Louie's postion is: " + animalArrayList.indexOf(louie));
-
-        // sort the animals - using a comparator; this is used to compare the elements for sorting
-        // comparator added in Animal class
-        animalArrayList.sort(Animal.AnimalNameComparator);
-
-        // after sorting
-        printAllAnimals(animalArrayList);
-
-        // find the position of the object in arraylist - indexof
-        System.out.println("Louie's postion is after sorting is: " + animalArrayList.indexOf(louie));
-
-    }
-
-    private static void printAllAnimals(ArrayList<Animal> animalArrayList) {
-        for (Animal a : animalArrayList) {
-            System.out.println(a);
-        }
+        makeAnimalsTalk();
+        System.out.println("Total value of animals " + calculateAssetValue(animals));
+        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
     }
 }
